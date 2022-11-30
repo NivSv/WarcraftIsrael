01fc1123a4195568828d450dd86d71855d1e33cd
Niv Stibel <Nivsvb@gmail.com>
2022-11-30 19:33:11 +0200

    added some auth and users logic

---
+++ backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios'
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    HttpModule,
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService, UsersService],
+})
+export class AppModule { }
