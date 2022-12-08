7096a9d39d25cb44451c890cc8e5e3912c2cf822
Niv Stibel <Nivsvb@gmail.com>
2022-12-08 13:39:25 +0200

    added test to interval service

--- backend/src/warcraft-data-interval/warcraft-data-interval.service.spec.ts
+++ backend/src/warcraft-data-interval/warcraft-data-interval.service.spec.ts
@@ -1,12 +1,17 @@
+import { ScheduleModule } from '@nestjs/schedule';
 import { Test, TestingModule } from '@nestjs/testing';
 import { WarcraftDataIntervalService } from './warcraft-data-interval.service';
+
+const scheduleModuleMock = {
+
+}
 
 describe('WarcraftDataIntervalService', () => {
   let service: WarcraftDataIntervalService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WarcraftDataIntervalService],
+      providers: [WarcraftDataIntervalService,{provide:ScheduleModule,useValue:scheduleModuleMock}],
     }).compile();
 
     service = module.get<WarcraftDataIntervalService>(WarcraftDataIntervalService);
