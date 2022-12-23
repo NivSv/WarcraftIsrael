a51a7ddc9e26eb689057ba35a5615e31069752a9
Niv Stibel <Nivsvb@gmail.com>
2022-12-23 16:08:59 +0200

    fix some lints

--- apps/wow-frontend/src/features/NavBar/MainNav.tsx
+++ apps/wow-frontend/src/features/NavBar/MainNav.tsx
@@ -1,8 +1,8 @@
 
 export default function MainNav() {
     return (
-        <nav className="relative mx-auto bg-primary-800">
-            <div className="flex justify-between items-center p-2">
+        <nav className="bg-primary-800 relative mx-auto">
+            <div className="flex items-center justify-between p-2">
                 <div className='flex items-center gap-2 text-2xl'>
                     <span>Main Nav</span>
                 </div>
--- apps/wow-frontend/src/features/NavBar/TopNav.tsx
+++ apps/wow-frontend/src/features/NavBar/TopNav.tsx
@@ -10,16 +10,16 @@ const socialMediaIcons = [
 
 export default function TopNav() {
   return (
-    <nav className="relative mx-auto bg-primary-900">
-        <div className="flex justify-between items-center p-2">
+    <nav className="bg-primary-900 relative mx-auto">
+        <div className="flex items-center justify-between p-2">
             <div className='flex items-center gap-2 text-2xl'>
-                <img className="object-scale-down h-12" src={logo} alt="logo" />
+                <img className="h-12 object-scale-down" src={logo} alt="logo" />
                 <span>Warcraft Israel</span>
             </div>
             <div>
                 {socialMediaIcons.map((icon, index) => {
                     return (
-                       <a href={icon.link} target="_blank"><FaFacebook/></a>
+                       <a href={icon.link} target="_blank" rel="noreferrer"><FaFacebook/></a>
                     )
                 })}
             </div>
