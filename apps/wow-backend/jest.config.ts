b9e01763825c8451315f093f20800382b7dbee56
Niv Stibel <Nivsvb@gmail.com>
2022-12-23 02:47:49 +0200

    change workspace folders

--- apps/wow-backend/jest.config.ts
+++ apps/wow-backend/jest.config.ts
@@ -12,5 +12,5 @@ export default {
         '^.+\\.[tj]s$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/packages/wow-backend',
+    coverageDirectory: '../../coverage/apps/wow-backend',
 }
